ae4a894a411730b9497e6f9b6815bec47e1a74bb
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2022-12-14 14:11:52 +0200

    Initial

--- app/src/main/java/com/udacity/asteroidradar/databse/DayProvider.kt
+++ app/src/main/java/com/udacity/asteroidradar/databse/DayProvider.kt
@@ -5,6 +5,9 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 object DayProvider {
+    /**
+     * Get Today in Format yyyy-MM-dd
+     */
     fun getToday(): String {
         val calendar = Calendar.getInstance()
         val currentTime = calendar.time
@@ -12,6 +15,9 @@ object DayProvider {
         return dateFormat.format(currentTime)
     }
 
+    /**
+     * Get Seven Days from Now in Format yyyy-MM-dd
+     */
     fun getSevenDaysLater(): String {
         val calendar = Calendar.getInstance()
         calendar.add(Calendar.DAY_OF_YEAR, 7)
