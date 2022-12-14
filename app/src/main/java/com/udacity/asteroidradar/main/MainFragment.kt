dd99627ad5d6f301465ed2ef4ac0ab4ff3022db9
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2022-12-14 10:52:43 +0200

    Initial

--- app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
@@ -2,14 +2,11 @@ package com.udacity.asteroidradar.main
 
 import android.os.Bundle
 import android.view.*
-import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.udacity.asteroidradar.AsteroidAdapter
 import com.udacity.asteroidradar.R
 import com.udacity.asteroidradar.databinding.FragmentMainBinding
@@ -22,8 +19,6 @@ class MainFragment : Fragment() {
     private lateinit var binding: FragmentMainBinding
     private lateinit var viewModel: MainViewModel
 
-    private lateinit var asteroidAdapter: AsteroidAdapter
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -33,17 +28,10 @@ class MainFragment : Fragment() {
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         binding.viewModel = viewModel
 
-        setHasOptionsMenu(true)
-
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        asteroidAdapter = AsteroidAdapter { asteroid ->
-            this.findNavController().navigate(MainFragmentDirections.actionShowDetail(asteroid))
+        val asteroidAdapter = AsteroidAdapter {
+            this.findNavController().navigate(MainFragmentDirections.actionShowDetail(it))
         }
 
-        binding.asteroidRecycler.layoutManager = LinearLayoutManager(requireContext())
         binding.asteroidRecycler.adapter = asteroidAdapter
 
 
@@ -54,38 +42,11 @@ class MainFragment : Fragment() {
         viewModel.loadingState.onEach { isLoading ->
             binding.statusLoadingWheel.isVisible = isLoading
         }.launchIn(lifecycleScope)
-    }
 
-//    override fun onCreateView(
-//        inflater: LayoutInflater,
-//        container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View {
-//        val binding = FragmentMainBinding.inflate(inflater)
-//        binding.lifecycleOwner = this
-//        binding.viewModel = viewModel
-//
-//        binding.asteroidRecycler.layoutManager = GridLayoutManager(context,1)
-////        val adapter = AsteroidAdapter(AsteroidAdapter.OnClickListener {
-////            findNavController().navigate(MainFragmentDirections.actionShowDetail(it))
-////        })
-//        val adapter = AsteroidAdapter { asteroid ->
-//            this.findNavController().navigate(MainFragmentDirections.actionShowDetail(asteroid))
-//        }
-//        binding.asteroidRecycler.adapter = adapter
-//
-////        viewModel.errorMessage.observe(viewLifecycleOwner) {
-////            Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
-////        }
-//
-//        viewModel.state.onEach { asteroidState ->
-//            adapter.setAsteroids(asteroidState.asteroids)
-//        }.launchIn(lifecycleScope)
-//
-//        setHasOptionsMenu(true)
-//
-//        return binding.root
-//    }
+        setHasOptionsMenu(true)
+
+        return binding.root
+    }
 
     @Deprecated("Deprecated in Java")
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
